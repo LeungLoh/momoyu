b73bbc546b9d0032da220e5f9f1abd7a5971b195
LeungLoh <843314152@qq.com>
2021-06-25 15:36:59 +0800

    Update settings.py

--- reptile/momoyu/momoyu/settings.py
+++ reptile/momoyu/momoyu/settings.py
@@ -86,6 +86,11 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+MYSQL_DB_NAME = ''
+MYSQL_HOST = ''
+MYSQL_PORT = 
+MYSQL_USER = ''
+MYSQL_PASSWORD = ''
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0'
 ITEM_PIPELINES={
